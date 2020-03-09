ebf43d1dbb555c61607136db7a7252575270c454
Shinji-MCZ <yoshinoya1234@gmail.com>
2020-03-09 16:06:58 +0900

    一旦

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ session_start();
 <body>
   <div class="flex-col-area">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
-      <a href="http:localhost/19_blog_system/index.php" class="navbar-brand">Camp Blog</a>
+      <a href="http://localhost/19_blog_system/index.php" class="navbar-brand">Camp Blog</a>
     <div class="collapse navbar-collapse" id="navbarToggle">
       <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
         <?php if ($_SESSION['id']) : ?>
